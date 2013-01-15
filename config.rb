b4fde696c578d60d65b86ae331ff0213b3cc8eed
Callum Strong <hello@callumstrong.co.uk>
2013-01-15 17:14:09 +0000

    Some post-winter housekeeping

---
+++ config.rb
@@ -0,0 +1,15 @@
+#  =============================================================================
+#  Config file for Compass app
+#  =============================================================================
+
+# The output style for the compiled css
+output_style = :compressed
+
+# The directory where the css stylesheets are kept. It is relative to the root
+css_dir = "css"
+
+# The directory where the images are kept. It is relative to the root
+images_dir = "img"
+
+# The directory where the javascripts are kept. It is relative to the root
+javascripts_dir = "js"
