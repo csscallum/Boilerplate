607c8f99fb61123893c897104f76d99d34bc9109
Callum Strong <hello@callumstrong.co.uk>
2013-10-07 21:18:28 +0100

    Added Grunt task runner
    Added Grunt task runner which features Sass compiler, image optimisation
    and uglify and concatenate JS files

---
+++ gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css' : 'sass/style.scss'
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/min/basket.min.js': ['js/basket.js'],
+                    'js/min/scripts.min.js': ['js/jquery.js','/js/script.js']
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                options: {
+                    progressive: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'sass/*.scss'
+                ],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'uglify', 'imagemin']);
+}
